82a431c4ad9078ccd5889a1fb5ac86b79769a3b0
May, Samuel <samuel.may@accenture.com>
2022-09-21 10:01:08 -0400

    starting state for cubes example

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import * as THREE from "three";
 import "./index.scss";
 
-// create our scene and add our perspective's camera
+// create our scene and add a perspective camera
 const scene = new THREE.Scene();
 const fov = 75;
 const aspect = window.innerWidth / window.innerHeight;
 const near = 0.1;
-const far = 5;
+const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
 // select the input element and set the size of what we are painting
@@ -21,43 +21,29 @@ const light_intensity = 1;
 const light = new THREE.DirectionalLight(light_color, light_intensity);
 light.position.set(-1, 2, 4);
 scene.add(light);
+const ambientLight = new THREE.AmbientLight(light_color, light_intensity / 2);
+scene.add(ambientLight);
 
-// create a cube and add it to our scene
-const geometry = new THREE.BoxGeometry();
-function makeCube(
-  color: THREE.ColorRepresentation,
-  x_coord: number
-): THREE.Mesh {
-  const material = new THREE.MeshPhongMaterial({ color: color });
-  // note that we use the same geometry to create three different mesh objects
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  cube.position.x = x_coord;
-  return cube;
-}
-const cubes = [
-  makeCube(0x44aa88, 0),
-  makeCube(0x8844aa, -2),
-  makeCube(0xaa8844, 2),
-];
-// scene.add(...cubes);
+// create some cubes and add them to our scene
+const box_geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
+// note that we use the same geometry to create three different mesh objects
+const cube1 = new THREE.Mesh(box_geometry, material);
+const cube2 = new THREE.Mesh(box_geometry, material);
+const cube3 = new THREE.Mesh(box_geometry, material);
 
-camera.position.z = 3;
+scene.add(cube1);
+scene.add(cube2);
+scene.add(cube3);
+cube1.position.x = 0;
+cube2.position.x = -2;
+cube3.position.x = 2;
 
-function render(time: number) {
-  // time to seconds
-  time *= 0.001;
-  const canvas = renderer.domElement;
-  camera.aspect = canvas.clientWidth / canvas.clientHeight;
-  camera.updateProjectionMatrix();
-  cubes.forEach((cube, i) => {
-    // slightly differ rotation for each cube
-    const speed = 1 + i * 0.1;
-    const rotation = time * speed;
-    cube.rotation.x = rotation;
-    cube.rotation.y = rotation;
-  });
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
-}
-requestAnimationFrame(render);
+// set our camera's position and render the scene
+camera.position.z = 5;
+camera.position.y = 2;
+camera.position.x = 1;
+camera.lookAt(0, 0, 0);
+camera.aspect = canvas.clientWidth / canvas.clientHeight;
+camera.updateProjectionMatrix();
+renderer.render(scene, camera);
